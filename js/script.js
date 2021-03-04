583fd1e54aa5ed4561f6e189b33eabb6f64d8b87
dove <ndoverte11@gmail.com>
2021-03-04 16:37:53 +0800

    Done animating featured section it used JS IntersectionObserver API for triggeringanimation

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ const navAnimation = (() => {
     navbar.classList.toggle("openMenuMobile");
   });
 })();
+
+const animateOnScroll = (() => {
+  const featurePostEl = document.querySelectorAll(".featured__post");
+  const FEATUREDPOSTOPTIONS = {
+    root: null,
+    rootMargin: "-250px 0px",
+    threshold: 0.25,
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    console.log(entries);
+    entries.forEach(({ isIntersecting }) => {
+      if (!isIntersecting) {
+        return;
+      } else {
+        console.log("execute");
+        featurePostEl.forEach((post) => {
+          post.classList.add("animate_up");
+        });
+      }
+    });
+  }, FEATUREDPOSTOPTIONS);
+
+  observer.observe(document.querySelector(".featured"));
+})();
